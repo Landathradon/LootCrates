b7b9c15631c4d7c85266f03bd4be98b22cc07f2d
armadyl <mastercape88@live.ca>
2017-11-05 01:17:47 -0500

    Initial commit

--- src/main/java/co/neweden/LootCrates/main.java
+++ src/main/java/co/neweden/LootCrates/main.java
@@ -1,10 +1,8 @@
 package co.neweden.LootCrates;
 
 import co.neweden.LootCrates.listeners.PlayerListener;
-import net.milkbowl.vault.permission.Permission;
 
 import org.bukkit.event.Listener;
-import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
 
@@ -13,7 +11,6 @@ public class main extends JavaPlugin implements Listener{
     // When the plugin load/unload
     @Override
     public void onEnable() {
-        setupPermissions();
         this.getServer().getPluginManager().registerEvents(new PlayerListener(), this);
         this.getLogger().info("LootBox is now running");
     }
@@ -22,20 +19,6 @@ public class main extends JavaPlugin implements Listener{
     public void onDisable() {
         this.getLogger().info("LootBox has now stopped");
     }
-
-    // setup vault permissions
-    public static Permission permission = null;
-
-    private boolean setupPermissions()
-    {
-        RegisteredServiceProvider<Permission> permissionProvider = getServer().getServicesManager().getRegistration(net.milkbowl.vault.permission.Permission.class);
-        if (permissionProvider != null) {
-            permission = permissionProvider.getProvider();
-        }
-        return (permission != null);
-    }
-
-
 
 
 }
